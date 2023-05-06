db9d7ca3e107c231df7b09f11ba8d7d32f1bd87a
wihctoh <rivertoxi911@gmail.com>
2023-05-06 14:12:08 +0300

    fix

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const playPauseBtn = document.querySelector(".songControll__playPauseBtn");
 const prevBtn = document.querySelector(".songControll__prevBtn");
 const nextBtn = document.querySelector(".songControll__nextBtn");
 const favBtn = document.querySelector(".songControll__favBtn");
+const spotifyBtn = document.querySelector(".songHeader__spotifyBtn");
 
 let flag = false;
 let currentIndexSong = 0;
+// duration: "05:48",
 
 const songs = [
   {
@@ -18,8 +20,8 @@ const songs = [
     artist: "Bullet for my Valentine",
     songName: "Tears Dont Fall",
     imgPath: "background-image: url(./assets/bfmvCover.jpg)",
-    duration: "05:48",
-    liked: "background-image: url(./assets/favBtnUsed.svg)",
+    duration: "00:20",
+    liked: false,
   },
   {
     id: 2,
@@ -28,7 +30,7 @@ const songs = [
     songName: "Parasite Eve",
     imgPath: "background-image: url(./assets/bmthCover.jpg)",
     duration: "04:51",
-    liked: "background-image: url(./assets/favBtnUsed.svg)",
+    liked: false,
   },
   {
     id: 3,
@@ -37,9 +39,35 @@ const songs = [
     songName: "Chop Suey!",
     imgPath: "background-image: url(./assets/soadCover.jpg)",
     duration: "03:30",
-    liked: "background-image: url(./assets/favBtnUsed.svg)",
+    liked: false,
   },
 ];
+
+function progressBar() {
+  const progress = document.querySelector(".progressBar__progress");
+  let minuts = 0;
+  let seconds = 0;
+  let count = 0;
+
+  let interval = setInterval(function () {
+    count++;
+    seconds++;
+
+    if (seconds >= "10") {
+      barTime.innerHTML = `0${minuts}:${seconds}`;
+    } else {
+      barTime.innerHTML = `0${minuts}:0${seconds}`;
+    }
+
+    if (seconds >= "60") {
+      minuts += 1;
+      seconds = 0;
+      barTime.innerHTML = `0${minuts}:0${seconds}`;
+    }
+
+    progress.style = `width: ${count}px`;
+  }, 1000);
+}
 
 function songInfo() {
   const currentSong = songs[currentIndexSong];
@@ -48,23 +76,47 @@ function songInfo() {
   artist.innerHTML = currentSong.artist;
   songName.innerHTML = currentSong.songName;
   songCover.style = currentSong.imgPath;
-  barTime.innerHTML = currentSong.duration;
+  // barTime.innerHTML = currentSong.duration;
+
+  if (currentSong.liked) {
+    favBtn.style = "background-image: url(./assets/favBtnUsed.svg)";
+  } else if (!currentSong.liked) {
+    favBtn.style = "background-image: url(./assets/favBtn.svg)";
+  }
+}
+
+function spotifyActive() {
+  if (!flag) {
+    spotifyBtn.style = "background-image: url(./assets/spotifyBtnActive.svg);";
+  } else if (flag)
+    spotifyBtn.style = "background-image: url(./assets/spotifyBtn.svg);";
 }
 
 favBtn.addEventListener("click", function () {
-  this.style = songs[currentIndexSong].liked;
+  if (!songs[currentIndexSong].liked) {
+    favBtn.style = "background-image: url(./assets/favBtnUsed.svg)";
+    songs[currentIndexSong].liked = true;
+  } else if (songs[currentIndexSong].liked) {
+    favBtn.style = "background-image: url(./assets/favBtn.svg)";
+    songs[currentIndexSong].liked = false;
+  }
 });
 
 playPauseBtn.addEventListener("click", function () {
   songInfo();
 
   if (!flag) {
+    spotifyActive();
     audio.play();
+    progressBar();
+
     this.style = "background-image: url(./assets/pauseBtn.svg);";
     flag = true;
   } else {
-    flag = false;
+    spotifyActive();
     audio.pause();
+
+    flag = false;
     this.style = "background-image: url(./assets/playBtn.svg);";
   }
 });
@@ -72,17 +124,27 @@ playPauseBtn.addEventListener("click", function () {
 prevBtn.addEventListener("click", function () {
   if (currentIndexSong === 0) return;
   currentIndexSong--;
-  songInfo(); 
+
+  songInfo();
+  spotifyActive();
   audio.play();
+
   playPauseBtn.style = "background-image: url(./assets/pauseBtn.svg);";
+  spotifyBtn.style = "background-image: url(./assets/spotifyBtnActive.svg);";
+
   flag = true;
 });
 
 nextBtn.addEventListener("click", function () {
   if (currentIndexSong === songs.length - 1) return;
   currentIndexSong++;
+
   songInfo();
+  spotifyActive();
   audio.play();
+
   playPauseBtn.style = "background-image: url(./assets/pauseBtn.svg);";
+  spotifyBtn.style = "background-image: url(./assets/spotifyBtnActive.svg);";
+
   flag = true;
 });
